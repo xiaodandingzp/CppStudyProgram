02d44a9a6aa2dd4d941234bd4391bc60bb94527c
zhangping2 <zhangping2@yy.com>
2022-07-26 20:56:45 +0800

    函数的分文件编写

---
+++ FirstConsoleApplication/first.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+#include "swap.h"
+
+//函数声明
+//函数可以声明多次
+int test();
+
+int main() {
+	cout << "zhangping is good" << endl;
+	int testa = test();
+	cout << testa << endl;
+	swap(10, 20);
+	return 0;
+}
+
+int test() {
+	return 1;
+}
