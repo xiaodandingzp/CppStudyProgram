89817f6331db506512ab4b0bd6a8f61e03ed16e7
xiaodandingzp <2440788861@qq.com>
2022-08-08 21:10:10 +0800

    类

--- FirstConsoleApplication/first.cpp
+++ FirstConsoleApplication/first.cpp
@@ -1,13 +1,27 @@
 #include<iostream>
 using namespace std;
 #include "swap.h"
+#include <string>
 
 //函数声明
 //函数可以声明多次
 int test();
+class Person {
+public:
+	string name;
+	Person(string name) {
+		this->name = name;
+	}
+
+	~Person() {
+		cout << "析构函数" << endl;
+	}
+};
+
 
 int main() {
-	funOverCite(10);
+	Person pp = Person("zhangping");
+	cout << "name:" << pp.name << endl;
 	return 0;
 }
 
